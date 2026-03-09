434c771c1816148551e17be72dd9b503bbe47475
mtronda21vekby-stack <m.tronda.21vek.by@gmail.com>
2026-03-08 23:57:48 -0400

    Update cms.module.ts

--- apps/api/src/modules/cms/cms.module.ts
+++ apps/api/src/modules/cms/cms.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CmsController } from './cms.controller';
+import { CmsService } from './cms.service';
 
 @Module({
-  controllers: [CmsController]
+  controllers: [CmsController],
+  providers: [CmsService],
+  exports: [CmsService]
 })
 export class CmsModule {}
